f04d7f43ad815e475eee3f54ee6f976d6da45dcf
kashikichi <60090083+Pikselas@users.noreply.github.com>
2023-10-09 00:02:03 +0530

    convertible to BoxCollider

--- Collision/SweepAndPrune.cpp
+++ Collision/SweepAndPrune.cpp
@@ -2,62 +2,68 @@
 #include<algorithm>
 #include<vector>
 #include<map>
+#include<type_traits>
 
 class BoxCollider
 {
-    private:
-        int left;
-        int right;
-        int top;
-        int bottom;
-    public:
-        BoxCollider( int min_point_left , int max_point_right , int max_point_top , int max_point_bottom  ) 
-            : 
-        left(min_point_left) , right(max_point_right) , top(max_point_top) , bottom(max_point_bottom) {}
-    public:
-        bool IsCollidingWith(const BoxCollider collider) const
-        {
-            bool x_overlap = left < collider.right && right > collider.left;
-            bool y_overlap = top < collider.bottom && bottom > collider.top;
-            return x_overlap && y_overlap;
-        }
-    public:
-        int GetLeft() const
-        {
-            return left;
-        }
-        int GetRight() const
-        {
-            return right;
-        }
-        int GetTop() const
-        {
-            return top;
-        }
-        int GetBottom() const
-        {
-            return bottom;
-        }
+private:
+    int left;
+    int right;
+    int top;
+    int bottom;
+public:
+    BoxCollider(int left, int right, int top, int bottom)
+        :
+        left(left), right(right), top(top), bottom(bottom) {}
+public:
+    bool IsCollidingWith(const BoxCollider collider) const
+    {
+        bool x_overlap = left < collider.right && right > collider.left;
+        bool y_overlap = top < collider.bottom && bottom > collider.top;
+        return x_overlap && y_overlap;
+    }
+public:
+    int GetLeft() const
+    {
+        return left;
+    }
+    int GetRight() const
+    {
+        return right;
+    }
+    int GetTop() const
+    {
+        return top;
+    }
+    int GetBottom() const
+    {
+        return bottom;
+    }
 };
 
+template<typename T>
+concept BoxColliderType = std::convertible_to<T, BoxCollider>;
+
+template<BoxColliderType T>
 class CollisionTester
 {
-    public:
-        static bool Predicator(const BoxCollider* a , const BoxCollider* b)
+public:
+    static bool Predicator(const T* a, const T* b)
+    {
+        return static_cast<BoxCollider>(*a).GetLeft() < static_cast<BoxCollider>(*b).GetLeft();
+    }
+
+    static std::vector<std::pair<T*, T*>> Test(std::vector<T*> colliders)
+    {
+        std::vector<std::pair<T*, T*>> collision_list;
+        std::sort(colliders.begin(), colliders.end(), Predicator);
+        for (unsigned int i = 0; i < colliders.size() - 1; ++i)
         {
-            return a->GetLeft() < b->GetLeft();
+            if (static_cast<BoxCollider>(*colliders[i]).IsCollidingWith(*colliders[i + 1]))
+                collision_list.emplace_back(colliders[i], colliders[i + 1]);
         }
-        static std::vector<std::pair<BoxCollider* , BoxCollider*>> Test(std::vector<BoxCollider*> colliders)
-        {
-            std::vector<std::pair<BoxCollider* , BoxCollider*>> collision_list;
-            std::sort(colliders.begin() , colliders.end() , Predicator);
-            for(unsigned int i = 0 ; i < colliders.size() - 1 ; ++i)
-            {
-                if(colliders[i]->IsCollidingWith(*colliders[i + 1]))
-                    collision_list.emplace_back(colliders[i] , colliders[i + 1]);
-            }
-            return collision_list;
-        }
+        return collision_list;
+    }
 };
 
 int main()
@@ -77,7 +83,7 @@ int main()
 
     std::vector<BoxCollider*> colliders = { &A , &B , &C , &D };
 
-    auto collision_list = CollisionTester::Test(colliders);
+    auto collision_list = CollisionTester<BoxCollider>::Test(colliders);
 
     for(auto& pair : collision_list)
     {
